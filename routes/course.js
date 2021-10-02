c0352139f6a002aa0fbb9182fa438e3c6a05b7fe
samarimam <samarimam78@gmail.com>
2021-10-02 16:12:40 +0530

    course route validation

--- routes/course.js
+++ routes/course.js
@@ -3,9 +3,29 @@ const courseController = require('../controllers/courseController');
 
 const router = express.Router();
 
-router.post('/create', courseController.createCourse);
+router.post(
+    '/create',
+    [
+        check('title').notEmpty(),
+        check('description').notEmpty(),
+        check('type').notEmpty(),
+        check('price').notEmpty(),
+        check('image').notEmpty(),
+    ],
+    courseController.createCourse
+);
 router.get('/courses', courseController.getAllCourses);
-router.post('/update/:id', courseController.updateCourse);
+router.post(
+    '/update/:id',
+    [
+        check('title').notEmpty(),
+        check('description').notEmpty(),
+        check('type').notEmpty(),
+        check('price').notEmpty(),
+        check('image').notEmpty(),
+    ],
+    courseController.updateCourse
+);
 router.delete('/delete/:id', courseController.deleteCourse);
 
 module.exports = router;
