f48a2042808b5ca1dd48911d6f9375e3c6d04240
arhamathar <arhamathar789@gmail.com>
2021-12-13 19:22:59 +0530

    edit referral points route added for admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const User = require('../models/user');
 const HttpError = require('../utils/httpError');
 
@@ -26,6 +27,7 @@ exports.getAllUsers = async (req, res, next) => {
                     title: '$courseInfo.title',
                     type: '$courseInfo.type',
                     price: '$courseInfo.price',
+                    points: '$referralPoints',
                 },
             },
         ]);
@@ -33,5 +35,45 @@ exports.getAllUsers = async (req, res, next) => {
         res.status(200).json({ status: 'Success', users });
     } catch (e) {
         next(new HttpError('Something went wrong, cannot find users!', 500));
+    }
+};
+
+exports.subtractReferralPoints = async (req, res, next) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return next(new HttpError('Entered email is invalid', 422));
+    }
+
+    const { email, referralPoints } = req.body;
+    try {
+        const redeemingUser = await User.findOne({ email });
+        if (!redeemingUser) {
+            return next(
+                new HttpError('User not found please check the email', 404)
+            );
+        }
+
+        if (redeemingUser.referralPoints < referralPoints) {
+            return next(
+                new HttpError(
+                    `User does not have ${referralPoints} points`,
+                    400
+                )
+            );
+        }
+
+        await User.updateOne(
+            { email },
+            { referralPoints: redeemingUser.referralPoints - referralPoints }
+        );
+        res.status(200).json({
+            status: 'success',
+            message: 'Points deducted successfully',
+        });
+    } catch (e) {
+        console.log(e);
+        return next(
+            new HttpError('Something went wrong, try again later', 500)
+        );
     }
 };
